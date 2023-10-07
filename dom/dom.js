25b974da8a965ffce9b966c185070fde488b084b
SINTU KUMAR <topcurrentaffairs09@gmail.com>
2023-10-08 00:08:36 +0530

    sintu2

--- dom/dom.js
+++ dom/dom.js
@@ -1,17 +1,59 @@
 
-const a = [0, 1, 2, 3, 4, 5, 6];
-const h1Elements = document.querySelectorAll("h1");
+// const a = [0, 1, 2, 3, 4, 5, 6];
+// const h1Elements = document.querySelectorAll("h1");
+
+// a.forEach((number, index) => {
+//     if (number % 2 === 0) {
+//         h1Elements[index].style.color = "green";
+//         h1Elements[index].style.background = "orange"
+
+//     } else {
+//         h1Elements[index].style.color = "orange ";
+//         h1Elements[index].style.background = "green"
 
 
-a.forEach((number, index) => {
-    if (number % 2 === 0) {
-        h1Elements[index].style.color = "green";
+//     }
+// });
+
+
+
+// const addButton = document.getElementById('addButton');
+// const taskInput = document.getElementById('taskInput');
+// const taskList = document.getElementById('taskList');
+
+// addButton.addEventListener('click', function () {
+//     const taskText = taskInput.value;
+//     if (taskText) {
+//         const taskItem = document.createElement('li');
+//         taskItem.textContent = taskText;
+//         // taskItem.addEventListener('click', function () {
+//         //     taskItem.style.color = 'green';
+//         // });
+
+//         taskList.innerHTML += '<li>' + taskText + '</li>';
+
+//         taskInput.value = '';
+//     }
+// });
+
+let i = 0;
+
+function print() {
+    let name = document.getElementById('name');
+    let x = name.value;
+
+    if (x.trim() !== '') {
+        let taskList = document.getElementById('taskList');
+        let listItem = document.createElement('li');
+        listItem.style.color = (i % 2 === 0) ? 'red' : 'blue';
+        listItem.appendChild(document.createTextNode(x));
+        taskList.insertBefore(listItem, taskList.firstChild);
+        i++;
+        name.value = '';
     } else {
-        h1Elements[index].style.color = "orange ";
+        alert('Please enter a task.');
     }
-});
-
-
+}
 
 
 
