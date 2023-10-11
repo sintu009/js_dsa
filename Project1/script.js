16f6a823360b5e54718fde485ef154840ed875ee
SINTU KUMAR <topcurrentaffairs09@gmail.com>
2023-10-11 23:38:31 +0530

    first commit

--- Project1/script.js
+++ Project1/script.js
@@ -1,22 +1,38 @@
-let inputs = document.getElementById("inputfield")
-
-let text = document.querySelector(".text")
-
+let tasks = [];
+let text = document.querySelector(".text");
 
 function Add() {
-    if (inputs.value == "") {
-        alert("Plz enter the task")
-    }
-    else {
-        let newEle = document.createElement("ul")
-        newEle.innerHTML = `${inputs.value} <i class="fa fa-trash" aria-hidden="true"></i>`;
-        text.appendChil(newEle)
-        inputs.value = "";
+    let inputs = document.getElementById("inputfield");
+    let task = inputs.value.trim();
 
-        newEle.querySelector("i").addEventListener("click", remove)
-        function remove() {
-            newEle.remove()
-        }
+    if (task === "") {
+        alert("Please enter the task");
+    } else {
+        tasks.unshift(task); // Add new task to the beginning of the array
+
+        // Clear previous tasks in the text element
+        text.innerHTML = "";
+
+        // Reverse the tasks array and render the tasks
+        tasks.forEach(function (task, index) {
+            let newEle = document.createElement("ul");
+            newEle.innerHTML = `${task} <i class="fa fa-trash" aria-hidden="true"></i>`;
+
+            if ((index + 1) % 2 === 0) {
+                newEle.style.backgroundColor = 'red';
+            } else {
+                newEle.style.backgroundColor = 'green';
+            }
+
+            text.appendChild(newEle);
+
+            newEle.querySelector("i").addEventListener("click", function () {
+                let index = tasks.indexOf(task);
+                tasks.splice(index, 1);
+                newEle.remove();
+            });
+        });
+
+        inputs.value = "";
     }
 }
-
